e021951a5777d7bc013a7b8507edee6acb105ef2
Usman Bangash <usmanbangash21@gmail.com>
2024-06-28 12:42:17 +0500

    first commit

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,5 +1,6 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
+import clsx from "clsx";
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
